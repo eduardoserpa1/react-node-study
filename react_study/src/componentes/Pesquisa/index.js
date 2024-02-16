94c435cb5754e2013b8d6d47db618c19e59ddd1e
eduardoserpa1 <dudu.rserpa@gmail.com>
2024-02-16 16:10:52 -0300

    added insert and delete items from 'favorito' and finished the 4th class and the course

--- react_study/src/componentes/Pesquisa/index.js
+++ react_study/src/componentes/Pesquisa/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { useState } from 'react'
 import { getLivros } from '../../servicos/livros'
 import { useEffect } from 'react'
+import { postFavorito } from '../../servicos/favoritos'
 
 
 const PesquisaContainer = styled.section`
@@ -63,6 +64,11 @@ function Pesquisa() {
        setLivros(livrosDaAPI)
    }
 
+   async function insertFavorito(id) {
+    await postFavorito(id)
+    alert(`Livro de id:${id} inserido!`)
+    }
+
 
    return (
        <PesquisaContainer>
@@ -77,7 +83,7 @@ function Pesquisa() {
                }}
            />
            { livrosPesquisados.map( livro => (
-               <Resultado>
+               <Resultado onClick={ ()=>insertFavorito(livro.id)}>
                    <img src={livro.src}/>
                    <p>{livro.nome}</p>
                </Resultado>
